7a92cd8d97869754a7c8fcde14480605e4ae3499
blake <blake@meishakeji.com>
2019-03-20 18:28:26 +0800

    feat: 整理代码

--- src/scene/SceneGame.ts
+++ src/scene/SceneGame.ts
@@ -13,8 +13,6 @@ class SceneGame extends eui.Component implements  eui.UIComponent {
 	private jumpVoice: egret.Sound;
 	// 舞台中的方块数组
 	private blockArr: Array<eui.Image> = [];
-	// 回收的方块数组
-	private reBackBlockArr: Array<eui.Image> = [];
 	// 当前的盒子（最新出现的盒子，就是准备要跳过去的目标盒子）
 	private currentBlock: eui.Image;
 	// 下一个的盒子方向（1靠右侧出现，-1靠左侧出现）
@@ -47,14 +45,6 @@ class SceneGame extends eui.Component implements  eui.UIComponent {
 	public overScoreLabel: eui.Label;
 	public restartBtn: eui.Button;
 
-	public static shared:SceneGame;
-	public static getInstance() {
-		if (!SceneGame.shared) {
-			SceneGame.shared = new SceneGame();
-		}
-		return SceneGame.shared;
-	}
-
 	public constructor() {
 		super();
 	}
@@ -71,9 +61,9 @@ class SceneGame extends eui.Component implements  eui.UIComponent {
 	}
 	
 	private init() {
-		// 所以盒子资源
+		// 所有盒子资源
 		this.blockSourceNames = ["block1_png", "block2_png", "block3_png"];
-		// 按下和跳跃的声音
+		// 加载按下和跳跃的声音
 		this.pushVoice = RES.getRes('push_mp3');
 		this.jumpVoice = RES.getRes('jump_mp3');
 		// 初始化场景（方块和小人）
@@ -82,7 +72,7 @@ class SceneGame extends eui.Component implements  eui.UIComponent {
 		this.blockPanel.touchEnabled = true;
 		this.blockPanel.addEventListener(egret.TouchEvent.TOUCH_BEGIN, this.onTapDown, this);
 		this.blockPanel.addEventListener(egret.TouchEvent.TOUCH_END, this.onTapUp, this);
-		// 心跳计时器（计算按的时长，推算出跳的距离）
+		// 心跳计时器（目的：计算按的时长，推算出跳的距离）
 		egret.startTick(this.computeDistance, this);
 	}
 
@@ -159,23 +149,17 @@ class SceneGame extends eui.Component implements  eui.UIComponent {
 
 	// 创建一个方块
 	private createBlock(): eui.Image {
-		var blockNode = null;
-		if (this.reBackBlockArr.length) {
-			// 回收池里面有,则直接取
-			blockNode = this.reBackBlockArr.splice(0, 1)[0];
-		} else {
-			// 回收池里面没有,则重新创建
-			blockNode = new eui.Image();
-		}
+		// 首先实例化一下
+		let blockNode = new eui.Image();
 		// 使用随机背景图
 		let n = Math.floor(Math.random() * this.blockSourceNames.length);
 		blockNode.source = this.blockSourceNames[n];
-		this.blockPanel.addChildAt(blockNode, 1);
-		// 设置方块的锚点
+		this.blockPanel.addChild(blockNode);
+		// 设置方块的锚点（之前说过的不是图片的中心点，而是图中盒子的中心点）
 		blockNode.anchorOffsetX = 222;
 		blockNode.anchorOffsetY = 78;
 		blockNode.touchEnabled = false;
-		// 把新创建的block添加进入blockArr里
+		// 把新创建的方块添加进入 blockArr 里，统一管理
 		this.blockArr.push(blockNode);
 		return blockNode;
 	}
@@ -228,8 +212,6 @@ class SceneGame extends eui.Component implements  eui.UIComponent {
 				// 方块超出屏幕,从显示列表中移除 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 				if (blockNode) this.blockPanel.removeChild(blockNode);
 				this.blockArr.splice(i, 1);
-				// 添加到回收数组中
-				this.reBackBlockArr.push(blockNode);
 			} else {
 				// 没有超出屏幕的话,则移动
 				egret.Tween.get(blockNode).to({
@@ -247,7 +229,6 @@ class SceneGame extends eui.Component implements  eui.UIComponent {
 		// 清空舞台
 		this.blockPanel.removeChildren();
 		this.blockArr = [];
-		this.reBackBlockArr = [];
 
 		// 进行初始化
 		this.initBlock();
@@ -269,6 +250,8 @@ class SceneGame extends eui.Component implements  eui.UIComponent {
 		this.player.anchorOffsetY = this.player.height - 20;
 		this.blockPanel.addChild(this.player);
 		// 初始化得分
+		this.score = 0;
+		this.scoreLabel.text = this.score.toString();
 		this.blockPanel.addChild(this.scoreLabel);
 		// 初始化方向
 		this.direction = 1;
@@ -276,14 +259,13 @@ class SceneGame extends eui.Component implements  eui.UIComponent {
 		this.addBlock();
 	}
 
-	// 添加 factor 的 set、get 方法，注意用 public
+	// 添加 factor 的 set、get 方法，注意用 public，factor 的初始值为 0
 	public get factor():number {
 		return 0;
 	}
 	// 计算方法参考二次贝塞尔公式：https://blog.csdn.net/korekara88730/article/details/45743339
 	// 这里的 getter 使 factor 属性从 0 开始，结合刚才 tween 中传入的 1，使其符合公式中的 t 的取值区间。
 	// 而重点是这里的 setter，里面的 player 对象是我们要应用二次贝塞尔曲线的显示对象，而在 setter 中给 player 对象的 xy 属性赋值的公式正是之前列出的二次贝塞尔曲线公式。
-	// 这里的 P0 点是(100,100)，P1 点是(300,300)，P2 点是(100,500)。
 	public set factor(t:number) {
 		this.player.x = (1 - t) * (1 - t) * this.player.x + 2 * t * (1 - t) * (this.player.x + this.targetPos.x) / 2 + t * t * (this.targetPos.x);
 		this.player.y = (1 - t) * (1 - t) * this.player.y + 2 * t * (1 - t) * (this.targetPos.y - 300) + t * t * (this.targetPos.y);
